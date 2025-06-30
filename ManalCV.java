f2ff33945da24da708fcfb4335611dd33c248ade
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-06-30 10:44:41 +0400

    Add ManalCV.java

---
+++ ManalCV.java
@@ -0,0 +1,20 @@
+public class ManalCV {
+    public static void main(String[] args){
+        System.out.println("Manal Al Hatmi");
+		System.out.println("Phone: +968-97555921");
+		System.out.println("Email: manalalhatmi@outlook.com");
+		System.out.println("Degree: B.Tech in Electronics and Telecommunications Engineering");
+		System.out.println("GPA: 3.26");
+		System.out.println("University: UTAS-Al Musannah");
+		System.out.println("skills: Python, Exel, PowerBI, Microsoft Azure, Networking, Cisco Packet Tracer, Data Analysis, HTML, CSS");
+        System.out.println("Training: Research Assistant/Sultan Qaboos University/TRA");
+        System.out.println("Training: Data Analysit and Visualization With Microsoft PowerBI/Makeen Nanodegree Program");
+        System.out.println("Training: Trainee/Petrolum Development Oman");
+        System.out.println("Training: Trainee/Royal Navy Of Omen");
+        System.out.println("Projects: EMF Exposure Measurements");
+        System.out.println("Projects: Fresh and Rotten Fruit Detection Using RaspberryPi and Python");
+        System.out.println("Projects: Web Application using Azure App Service");
+        System.out.println("Projects: Creat an executive level power BI report comparing performance between two smart pet fitness devices for dog and cat");
+        System.out.println("Languages: Arabic, English");
+    }
+}
